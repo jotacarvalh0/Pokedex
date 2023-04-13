ff57022b014ecdea2fbed5eb700969c86e4fff45
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-04-13 11:10:51 -0300

    Manipulando resultado da requisição com promise.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
-fetch(url).then(function (response){
-    console.log(response)
-})
+fetch(url) /* Por padrão o fetch utiliza o get. */
+    .then((response) => response.json()) /* Transformando o body da api em Json. */
+    .then((jsonBody) => console.log(jsonBody)) /* Retornando a resposta da api. */
+    .catch((error) => console.log(error)) /* Se der erro na requisição*/
