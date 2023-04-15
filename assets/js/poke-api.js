33682a28835486af6c861f14a05aa442e7e114e8
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-04-15 16:02:47 -0300

    Manipulando múltiplas requisições em paralelo.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,11 +3,17 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json()) /* "fetch(pokemon.url)" novo Fetch para a url do pokemon */
+}                                                                 /* ".then((response) => response.json()" Convertendo a resposta do novo Fetch para json. */
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
     return fetch(url)
         .then((response) => response.json()) /* Transformando o body da api em Json. */
-        .then((jsonBody) => jsonBody.results) /* Retornando a resposta da api. */
-        .catch((error) => console.error(error)) /* Se der erro na requisição*/
+        .then((jsonBody) => jsonBody.results) /* Retornando a resposta apenas a parte do results. */
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) /* Mapeando os pokemons em uma lista de detalhes de pokemons. */
+        .then((detailRequests) => Promise.all(detailRequests)) /* "Promise.all(detailRequests))" Esperar que todas as requisições terminem. */
+        .then((pokemonsDetails) => pokemonsDetails)
     }
-
